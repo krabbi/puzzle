a8a13bf00075bb0983471fd78d653d6adcb5843c
krabbi <krabbiwine@gmail.com>
2018-06-11 12:11:01 +0300

    Update constants.js
    smaller squares

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 export const CANVAS_HEIGHT = innerHeight;
 export const CANVAS_WIDTH = innerWidth;
-export const RECTANGLE_HEIGHT = 150;
+export const RECTANGLE_HEIGHT = 100;
 export const SHOWN_FIGURES_NUMBER = 3;
 export const DROP_ACCURACY = 10;
 export const IMAGE_SOURCE = 'res/imgs/waterfall.jpg';
